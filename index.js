1b41073ee6494262d21affd2975711a9c72f7eea
RahulRamanDash <dashrahul193@gmail.com>
2024-02-09 11:24:23 +0530

    no changes

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
+const express = require('express');
+const mysql = require('mysql2');
+const path = require('path');
 
-const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const PORT = 3000;
 
-app.listen(port, () => {
-    console.log(`Example app listening on port http://127.0.0.1:${port}`)
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'infinite_august23',
 });
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+app.get('/employ', (req, res) => {
+    db.query('SELECT * FROM employ', (err, results) => {
+      if (err) throw err;
+      res.json(results);
+    });
+  });
+
+  // Get a user by ID
+app.get('/employ/:id', (req, res) => {
+    const { id } = req.params;
+    let errMsg = {"error":"Invalid Employ Id"};
+    db.query('SELECT * FROM employ WHERE Empno = ?', [id], (err, results) => {
+      if (err) throw err;
+      if(results.length > 0){
+        res.json(results[0]);
+    }else{
+          res.json(errMsg);
+      }
+    });
+  });
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
-
-app.get('/login', (req, res) => {
-    res.send('<h1>this is the login page</h1>')``
-});
-app.use(express.static('public'));
-
-// Add a new route for the /about page
-app.get('/about', (req, res) => {
-  res.send('<h1>About Us</h1><p>We are a team of developers passionate about Node.js and Express.js.</p>');
-});
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname + '/public' +'/index.html'));
+  });
